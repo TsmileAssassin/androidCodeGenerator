fb0f720da4a22aac5205c9b9a44da2d84787d982
chexing <superchexing@gmail.com>
2014-01-15 16:25:51 +0800

    del not used

--- codeGeneratorSample/src/main/java/com/example/codeGenerator/TestGeneratorMain.java
+++ codeGeneratorSample/src/main/java/com/example/codeGenerator/TestGeneratorMain.java
@@ -5,10 +5,10 @@ import com.tsmile.codeGenerator.Generator;
 public class TestGeneratorMain {
     public static void main(String[] args) throws Exception {
         Generator generator = new Generator();
-        generator.generateActivity( "res//layout//act_test.xml", "TestActivity",
+        generator.generateActivity(System.getProperty("user.dir") + "//res//layout//act_test.xml", "TestActivity",
                 "com.example.codeGenerator");
 
         Generator generator2 = new Generator();
-        generator2.generateFindViewCode("res//layout//act_test.xml", "TestFindView");
+        generator2.generateFindViewCode(System.getProperty("user.dir") + "//res//layout//act_test.xml", "TestFindView");
     }
 }
